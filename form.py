3edeb6c3cef875becfdcc1ddae1492bd0891bf73
joey <joeydepauw@live.be>
2017-08-14 20:54:38 +0200

    .

--- form.py
+++ form.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, TextAreaField
 
 class ConfessionForm(FlaskForm):
-    confession = TextAreaField('Confession', [validators.Length(min=10)], type="", render_kw={"style": "resize: vertical;", "rows": 10})
+    confession = TextAreaField('Confession', [validators.Length(min=10)], render_kw={"style": "resize: vertical;", "rows": 10})
     # accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
