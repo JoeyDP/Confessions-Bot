cac9738ca8842a74034d2340e67263a37197216a
Joey De Pauw <joeydepauw@gmail.com>
2018-08-03 13:32:21 +0200

    added forward code

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ from database import Confession, Page, Base
 from facebook import FBPage
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
-from flask import Flask, request, g, render_template, redirect, url_for, abort
+from flask import Flask, request, g, render_template, redirect, url_for, abort, Response
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
 from rq.decorators import job
+
+import requests
 
 import worker
 rqCon = worker.conn
@@ -165,6 +167,11 @@ def receivedRequest(request):
                 sender = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                 recipient = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
 
+                # Don't mind this piece of code. It forwards requests from specific pages to another bot.
+                if recipient in ["942723909080518", "1911537602473957"]:
+                    forward.delay('https://party-post.herokuapp.com/messenger')
+                    return
+
                 if messaging_event.get("message"):  # someone sent us a message
                     message = messaging_event["message"].get("text")
                     if not message:
@@ -175,6 +182,18 @@ def receivedRequest(request):
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     payload = messaging_event["postback"]["payload"]  # the message's text
                     receivedPostback.delay(sender, recipient, payload)
+
+
+@job('low', connection=rqCon)
+def forward(destinationUrl):
+    """ Forward requests to other bot. Source: https://stackoverflow.com/a/36601467 """
+    resp = requests.request(
+        method=request.method,
+        url=destinationUrl,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
 
 
 @job('low', connection=rqCon)
